fb2cc95d89eaa6780d2e8ce5f03b96614a0f7cdf
AyobamiMasterpiece <ayotibami14@gmail.com>
2022-09-11 08:09:06 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-nclude<stdio.h>
+#include<stdio.h>
 /**
  *  * main - coding
  *   *Description: pregramming
  *    * Return: 0
- *     */
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
